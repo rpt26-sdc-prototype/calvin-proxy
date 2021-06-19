5a9240bc56776e4313599d20ed31c31af847477d
Melanie DeWitt <melaniebd1986@gmail.com>
2021-06-19 15:23:12 -0600

    updated k6 testing file

--- stressTesting/k6get.js
+++ stressTesting/k6get.js
@@ -4,19 +4,16 @@ import { sleep } from 'k6';
 export let options = {
   stages: [
     { duration: '1m', target: 1 }, // Get up to 1 rps
-    { duration: '2m', target: 1 }, // stay there for 2 minutes
-    { duration: '2m', target: 10 }, // get up to 10rps
-    { duration: '2m', target: 10 }, // stay
-    { duration: '2m', target: 100 }, // get up to 100 rps
-    { duration: '2m', target: 100 }, // stay
-    { duration: '2m', target: 1000 }, // get up to 1000 rps
-    { duration: '2m', target: 1000 }, // stay
+    { duration: '1m', target: 10 }, // get up to 10rps
+    { duration: '1m', target: 100 }, // get up to 100 rps
+    { duration: '1m', target: 1000 }, // get up to 1000 rps
+    { duration: '1m', target: 1000 }, // stay
     { duration: '5m', target: 0 }, // scale down. Recovery stage.
   ],
 };
 
 export default function() {
-  const BASE_URL = 'http://localhost:3000';
+    const BASE_URL = 'http://3.20.89.16:3000';
   let responses = http.batch([
     [
       'GET',
